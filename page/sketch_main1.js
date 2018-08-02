25c1fc0e8b7e5f1a8033921bdf37af8f2b0252a5
Mukun Guo <gmk19990312@gmail.com>
2018-08-02 10:06:36 -0700

    final version

--- page/sketch_main1.js
+++ page/sketch_main1.js
@@ -30,23 +30,23 @@ var last_print;
 
 // var gameover_plot = new Node('game_over','Game over, thank you for playing our games!','Merchant Chen','t',true);
 
-var t_plot1 = new Node('t_p1', 'My lord, some of our population is starving at the north side of our kingdom, what should we do?', 'Demon Steward', 't', true);
+var t_plot1 = new Node('t_p1', 'My lord, some of our population are starving at the north side of our kingdom, what should we do?', 'Demon Steward', 't', true);
 var t_plot1_left = new Node('Give them some food.', 'Give them some food, I don\'t want to use these small bones for my magic, I want strong bones!', 'Norrix Seros', 't',0,5,-5,10,-15,0,0,0,0);
 var t_plot1_right = new Node('Let them die and make them soldiers', 'Let them die, I need some bones for my undead magic, don\'t give them anything, I will collect their bones soon.', 'Norrix Seros', 't',0,-10,10,-10,5,0,0,0);
 
-var z_plot1 = new Node('z_p1', 'My dear Paladin, there is some priest outside the castle who want to build some churches in our country... ', 'Commandar Sehotos', 'z', true);
-var z_plot1_left = new Node('Let the priests in', 'Welcome to my country, my honorable guests, here is the money for the church, I hope you can use the power of faith to strengthen the military.', 'Paladin Arthurius the Noble', 'z',0,0,0,0,-10,0,5,10);
+var z_plot1 = new Node('z_p1', 'My dear Paladin, there are some priests outside the castle who want to build a church in our country... ', 'Commandar Sehotos', 'z', true);
+var z_plot1_left = new Node('Let the priests in', 'Welcome to my country, my honorable guests, here is the money for the church, I hope you can use the power of faith to strengthen our military.', 'Paladin Arthurius the Noble', 'z', 0,0,0,0,0,-10,0,5,10);
 var z_plot1_right = new Node('Send the priests out of the country', 'My soldiers are undefeatable, they don\'t need to believe in any stupid gods or goddesses, let those priests out, they come here for money.', 'Paladin Arthurius the Noble', 'z',0,0,0,0,0,5,-5,0,-5);
 
 var t_plot2 = new Node('t_p2','We found a vampire in the kingdom, who wants to sell some blood to you so that you can use them for your undead magic, do you want to see him?','Demon Steward','t',true);
 var t_plot2_left = new Node('Make him into my castle','I want your blood, sell me some vampire!','Norrix Seros','t',0,-5,5,5,-10,0,0,0,0,0);;
 var t_plot2_right = new Node('Kill him!','Kill him! I don\'t want to see those monster who cannot live under the sun.','Norrix Seros','t',0,5,-5,-5,10,0,0,0,0);
 
-var z_plot2 = new Node('z_plot2','Dear lord, I heard people in the city talking about your illegitimate child... What should I do?','Commandar Sehotos','z',true);
-var z_plot2_left = new Node('Allow them','I don\'t mind people talking about this, I don\'t have any children at all, they can talk whatever they like, I don\'t care.','Paladin Arthurius the Noble','z',0,0,0,0,0,0,+5,0,-10);
-var z_plot2_right = new Node('I don\'t want to hear these things anymore','Kill those who say things like this, I don\'t have any children at all, I will not allow these gossips pass around in the city, kill them, now.','Paladin Arthurius the Noble','z',0,0,0,0,0,-5,-5,-5,5);
+var z_plot2 = new Node('z_plot2','Dear lord, I heard people in the city talking about your illegitimate son... What should I do?','Commandar Sehotos','z',true);
+var z_plot2_left = new Node('Allow them','I don\'t mind people talking about this, I don\'t have any sons at all, they can talk whatever they like, I don\'t care.','Paladin Arthurius the Noble','z',0,0,0,0,0,0,5,0,-10);
+var z_plot2_right = new Node('I don\'t want to hear these things anymore','Kill those who say things like this, I don\'t have any sons at all, I will not allow these gossips pass around in the country, kill them, now.','Paladin Arthurius the Noble','z',0,0,0,0,0,-5,-5,-5,5);
 
-var t_plot3 = new Node('t_p3','Dear Lich King, I am Dewey Ellis, brother of the bthe vampire who wanted to sell you blood one week ago, I require to stay in your country, and I can make more blood from people of the kingdom, what do you think, my lord?','Vampire Dewey Ellis','t',true);
+var t_plot3 = new Node('t_p3','Dear Lich King, I am Dewey Ellis, brother of the the vampire who wanted to sell you sometimes ago. I want to stay in your country, and I can make more blood from people of the kingdom, what do you think?','Vampire Dewey Ellis','t',true);
 var t_plot3_left = new Node('Let him stay.','Stay in my country, my friend, I can give you money if you give me blood like today, I can support your hunting!','Norrix Seros','t',0,-5,5,5,-10,0,0,0,0);
 var t_plot3_right = new Node('Get out of my country','Your are done now, I don\'t need your stupid blood anymore, get out of my country or I will kill you, now.','Norrix Seros','t',0,10,0,-5,0,0,0,0,0);
 
@@ -56,15 +56,15 @@ var z_plot3_right = new Node('Accept the money','Oh, that\'s what I should do, I
 
 var t_plot4 = new Node('t_p4','My Lord, we have just come out with some new magic to control people, but it needs a lot of money and oblation.','Devil Wizard Committe','t',true);
 var t_plot4_left = new Node('Pay for the magic and learn it','Let me learn it, I want my people to act like slaves and only listen to me, including you and your committee.','Norrix Seros','t',0,5,10,-5,-5,0,0,0,0);
-var t_plot4_right = new Node('Refuse the magic','Get out of here before I kill you, how tired is it to control everybody, I just kill those who don\'t obey me.','Norrix Seros','t',0,-5,-5,-5,0,0,0,0,0);
+var t_plot4_right = new Node('Refuse the magic','Get out of here before I kill you, how tired is it to control everybody, I just kill those who don\'t obey me.','Norrix Seros','t',0,-5,-5,5,0,0,0,0,0);
 
 
 var z_plot4 = new Node('z_p4','A disease is found on a boat and it is possible to be a plague, should we isolate the whole port?','Commandar Sehotos','z',true);
 var z_plot4_right = new Node('Isolate the whole port','Isolate them, don\'t let the plague ruin our country.','Paladin Arthurius the Noble','z',0,0,0,0,0,-5,-10,-5,0);
-var z_plot4_left = new Node('It can not be a plague, let it go','Let it go, it cannot be a plague, just a little disease, it will not do any harm to our country.','z',0,0,0,0,0,-10,-10,0,0);
+var z_plot4_left = new Node('It can not be a plague, let it go','Let it go, it cannot be a plague, just a little disease, it will not do any harm to our country.','Paladin Arthurius the Noble','z',0,0,0,0,0,-10,-10,0,0);
 
 var t_plot5 = new Node('t_p5','My lord, I want to build a wizard school for those talented youth.','Devil Wizard Committe','t',true);
-var t_plot5_left = new Node('Build the school','Build it, I want thouse talented people, when they graduate, they can help me to control the army.','Norrix Seros','t',0,5,10,5,-20,0,0,0,0);
+var t_plot5_left = new Node('Build the school','Build it, I want thouse talented people, when they graduate, they can help me to control the army.','Norrix Seros','t',0,5,10,5,-15,0,0,0,0);
 var t_plot5_right = new Node('Do not waste my money','No school for those who will die one day, their only mission is to fight for me as a bone!','Norrix Seros','t',0,-5,-5,-5,0,0,0,0,0);
 
 var z_plot5 = new Node('z_p5','My Noble Paladin. A bounch of military betrayed us, what should we do?','Commandar Sehotos','z',true);
@@ -83,20 +83,63 @@ var t_plot7 = new Node('t_p7','My lord, as you kill so many people, I look from 
 var t_plot7_left = new Node('Yes, get rid of it for me','Get rid of it, my dear friend, I don\'t wanna die so early','Norrix Seros','t',0,5,0,0,-10,0,0,0,0);
 var t_plot7_right = new Node('You lier','Is there anything a magician cannot foresee, you are lying to me, you just want money, I know it well, you greedy people!','Norrix Seros','t',0,-10,0,-5,0,0,0,0,0);
 
-var z_plot7 = new Node('z_p7','PDear lord, I am Pirate Jack Spiro, we are tired of the life on the sea, we want to join you to be your soldier.','Pirate Jack Spiro','z',true);
-var z_plot7_left = new Node('Accept them','I want you to be my friends, however, you have killed many of my people and now you want to join us, do you think you should sacrifice something?','Paladin Arthurius the Noble','z',0,0,0,0,0,)
+var z_plot7 = new Node('z_p7','Dear lord, I am Pirate Kunkka, we are tired of the life on the sea, we want to join you to be your soldier.','Pirate Kunkka','z',true);
+var z_plot7_left = new Node('Accept them and ask them for treasure','I want you to be my friends, however, you have killed many of my people and now you want to join us, do you think you should sacrifice something?','Paladin Arthurius the Noble','z',0,0,0,0,0,10,5,5,-5);
+var z_plot7_right = new Node('Let them join in without any price','Paladin Arthurius the Noble: Zoash welcome you, you will be our best friends from now on, I can forgive you for killing my people before.','Paladin Arthurius the Noble','z',0,0,0,0,0,-5,10,10,-5);
 
+var t_plot8 = new Node('t_p8','My lord, the price of the food in the kingdom is too low, should we raise it up?','Demon Steward','t',true);
+var t_plot8_left = new Node('Yes, raise the price','Raise the price, how can I let those people live a welthy life, all they do is turning to bones and let me use them.','Norrix Seros','t',0,-5,0,-10,10,0,0,0,0);
+var t_plot8_right = new Node('No, keep it the same prive','Keep it the same, I have enough money to use, sometimes I should think a little bit for my people.','Norrix Seros','t',0,5,0,10,-10,0,0,0,0);
+
+var z_plot8 = new Node('z_p8','My lord, I want to sell you some weapons for your army, so that your army will be undefeatable.','Goblin Omar Murphy','z',true);
+var z_plot8_left = new Node('Accept the weapons','Thank you for your weapon, with your things, my army can be much stronger.','Paladin Arthurius the Noble','z',0,0,0,0,0,-10,5,10,-5);
+var z_plot8_right = new Node('Refuse the Goblin','I don\'t need your weapon, my kingdom already has the most powerful armies in the whole cosmos.','Paladin Arthurius the Noble','z',0,0,0,0,0,5,-5,-5,0);
+
+var t_plot9 = new Node('t_p9','Dear lord, we find some gold under the river, what do you want do with this money?','Demon Steward','t',true);
+var t_plot9_left = new Node('Share with my people','Share the money among the country, this is what my people deserve.','Norrix Seros','t',0,5,0,5,5,0,0,0,0);
+var t_plot9_right = new Node('Share the money with Demon Steward.','I want to share my money with you, my dear Steward, for all the time, you help me a lot, this is what you deserve','Norrix',0,-10,5,2,10,0,0,0,0);
+
+var z_plot9 = new Node('z_p9','Dear Lord, we need money to organize the annual Spring festival, it will be amazing this year!','Spring Maiden','z',true);
+var z_plot9_left = new Node('Don\'t make those stupid event','Do not make the stupid event, it is just a waste of money, no one wants to attend somewhat called the Spring festival, is sounds boring!','Paladin Arthurius the Noble','z',0,0,0,0,0,0,-10,0,0);
+var z_plot9_right = new Node('Hold the event','Spring festival, sounds nice, let\'s hold the event, but I want to make sure everybody is safe, so militaries force is unavoidable.','Paladin Arthurius the Noble','z',0,0,0,0,0,-10,10,-5,5);
+
+var t_plot10 = new Node('t_p10','My lord, the war lord of the East would like to meet you. He can crush our armies if he wants, we should welcome him like a king.','Demon Steward','t',true);
+var t_plot10_left = new Node('Let him go, I don\'t want to see him.','I don\'t want to see him, he is so proud of himself, looks like a noob.','Norrix Seros','t',0,0,-10,0,-10,0,0,0,0);
+var t_plot10_right = new Node('Welcome him like a king','Although I don\'t want to do that, but his power is so large, I must think for my people.','Norrix Seros','t',0,-10,-5,0,-5,0,0,0,0);
+
+var z_plot10 = new Node('z_p10','Bad news! The Cosmos pirates are attaking us, what should we do? Should we attack or defend?','Commandar Sehotos','z',true);
+var z_plot10_left = new Node('Attack back','Attack them, kill them all!','Paladin Arthurius the Noble','z',0,0,0,0,0,-5,-10,-5,10);
+var z_plot10_right = new Node('Defence, save the people','Save the people first, I don\'t want anybody to die.','Paladin Arthurius the Noble','z',0,0,0,0,0,-10,-2,5,-5);
+
+
+var t_plotend1 = new Node('t_p11','My father, I was falling in love with the princess in another country call Zoash, however, I think she doesn\'t like me as I like her, what do you think about this, dad? Can I marry her?','Demon Prince Alexios','t',true);
+var t_plotend1_left = new Node('Refuse this love','How can you fall in love with a normal human being, you are the greatest wizard, you will control the country after I pass away! You are so irresponsible!','Norrix Seros','t',0,-10,0,-5,0,0,0,0,0);
+var t_plotend1_right = new Node('Rob the princess of Zoash!','You are the son of the greatest king the the whole universe, Alexios, how can you love a women who doesn\'t love you. Let me grab her here and you can ask agian','Norrix Seros','t',0,0,-10,-10,-10,0,0,0,0);
+
+var z_plotend1 = new Node('z_p11','My father, there is a wizard in Tririlia who is very handsome, I think... I may be fall in love with him, but I don\'t know wether he loves me or not...','Princess Anastasia','z',true);
+var z_plotend1_left = new Node('Refuse the love','How can the daughter of Paladin fall in love with a wizard, I don\'t want to hear of this thing again.','Paladin Arthurius the Noble','z',0,0,0,0,0,0,-5,0,-10);
+var z_plotend1_right = new Node('Make the wizard into my country, I want to see him','It is the first time my daughter fall in love, I want to see what the man like and let her abandon this love, grab him to me, my Commandar!','Paladin Arthurius the Noble','z',0,0,0,0,0,-5,0,-10,0);
+
+var t_plotend2 = new Node('t_p12','Don\'t do that my father, it is not worth that you are so angry about it, I just give it up, it is OK','Demon Prince Alexios','t',true);
+var t_plotend2_left = new Node('Go and find her','Although my father refuse my love, but my life is not his, I can control my life, let me go and find you, my beautiful lady.','Demon Prince Alexios','t',0,0,0,0,-5,0,0,0,0);
+var t_plotend2_right = new Node('Go and find her','Although my father refuse my love, but my life is not his, I can control my life, let me go and find you, my beautiful lady.','Demon Prince Alexios','t',0,0,0,0,-5,0,0,0,0);
+
+var z_plotend2 = new Node('z_p12','My dear father, you dom\'t have to do so many things for me, I can give up this love myself, you are not worth to worry about so many things, you should pay more attention to the things that threaten the country.','Princess Anastasia','z',true);
+var z_plotend2_left = new Node('Go and find him','Although my father don\'t support this love, I believe that he is the only one I can spend my life with, I will go and find him myself','Princess Anastasia','z',0,0,0,0,0,0,-5,0,0);
+var z_plotend2_right = new Node('Go and find him','Although my father don\'t support this love, I believe that he is the only one I can spend my life with, I will go and find him myself','Princess Anastasia','z',0,0,0,0,0,0,-5,0,0);
+
+var endplot = new Node('end','From then on, Demon Prince Alexios and Princess Anastasia live happlily ever after','Merchant Chen','t',true);
 
 var currentNode = t_plot1;
 if (choice == 'z') {
   currentNode = z_plot1;
   $(document).ready(function() {
-  $('div#audio').html('<audio src="assets/firewatch.mov" autoplay=1></audio>');
+  $('div#audio').html('<audio src="assets/Firewatch (Full OST).mp3" autoplay=1></audio>');
   });
 }
 else if (choice == 't') {
   $(document).ready(function() {
-  $('div#audio').html('<audio src="assets/journey.mov" autoplay=1></audio>');
+  $('div#audio').html('<audio src="assets/Journey OST - Complete Soundtrack.mp3" autoplay=1></audio>');
   });
 }
 
@@ -157,6 +200,52 @@ linkNode(t_plot7, t_plot7_left, t_plot7_right);
 linkNode(t_plot7_left, z_plot7);
 linkNode(t_plot7_right, z_plot7);
 
+linkNode(z_plot7, z_plot7_left, z_plot7_right);
+linkNode(z_plot7_left, t_plot8);
+linkNode(z_plot7_right, t_plot8);
+
+linkNode(t_plot8, t_plot8_left, t_plot8_right);
+linkNode(t_plot8_left, z_plot8);
+linkNode(t_plot8_right, z_plot8);
+
+linkNode(z_plot8, z_plot8_left, z_plot8_right);
+linkNode(z_plot8_left, t_plot9);
+linkNode(z_plot8_right, t_plot9);
+
+linkNode(t_plot9, t_plot9_left, t_plot9_right);
+linkNode(t_plot9_left, z_plot9);
+linkNode(t_plot9_right, z_plot9);
+
+linkNode(z_plot9, z_plot9_left, z_plot9_right);
+linkNode(z_plot9_left, t_plot10);
+linkNode(z_plot9_right, t_plot10);
+
+linkNode(t_plot10, t_plot10_left, t_plot10_right);
+linkNode(t_plot10_left, z_plot10);
+linkNode(t_plot10_right, z_plot10);
+
+linkNode(z_plot10, z_plot10_left, z_plot10_right);
+linkNode(z_plot10_left, t_plotend1);
+linkNode(z_plot10_right, t_plotend1);
+
+linkNode(t_plotend1, t_plotend1_left, t_plotend1_right);
+linkNode(t_plotend1_left, z_plotend1);
+linkNode(t_plotend1_right, z_plotend1);
+
+linkNode(z_plotend1, z_plotend1_left, z_plotend1_right);
+linkNode(z_plotend1_left, t_plotend2);
+linkNode(z_plotend1_right, t_plotend2);
+
+linkNode(t_plotend2, t_plotend2_left, t_plotend2_right);
+linkNode(t_plotend2_left, z_plotend2);
+linkNode(t_plotend2_right, z_plotend2);
+
+linkNode(z_plotend2, z_plotend2_left, z_plotend2_right);
+linkNode(z_plotend2_left, endplot);
+linkNode(z_plotend2_right, endplot);
+
+
+
 
 
 
@@ -182,7 +271,11 @@ function preload() {
   Vampire = loadImage('assets/Vampire.png');
   Star_Gazer = loadImage('assets/Star_Gazer.png');
   Pirate = loadImage('assets/Pirate.jpg');
-  Devil_Commiitee = loadImage('assets/Evil_Wizard.png')
+  Devil_Commiitee = loadImage('assets/Evil_Wizard.jpg');
+  Merchant_Chen = loadImage('assets/Merchant_Chen.jpg');
+  Spring_Maiden = loadImage('assets/Spring_maiden.jpeg');
+  Demon_prince = loadImage('assets/Demon_prince.jpg');
+  Princess = loadImage('assets/princess.jpeg');
 
   characters={
     'Norrix Seros': Lich_King,
@@ -192,7 +285,12 @@ function preload() {
     'Vampire Dewey Ellis': Vampire,
     'Devil Wizard Committe': Devil_Commiitee,
     'Stargazer Frank Mitchell': Star_Gazer,
-    'Pirate Jack Spiro': Pirate
+    'Pirate Kunkka': Pirate,
+    'Merchant Chen': Merchant_Chen,
+    'Spring Maiden': Spring_Maiden,
+    'Demon Prince Alexios': Demon_prince,
+    'Princess Anastasia': Princess,
+    'Goblin Omar Murphy': Goblin
   };
 
 }
@@ -312,6 +410,12 @@ function setup() {
 
 
 function draw() {
+  if (currentNode.atitle === 'end') {
+  	console.log('end');
+    setTimeout(function() {
+      window.location = 'ending.html?target=win&level=0';
+    }, 6000);
+  }
   // setGradient(0, 0, width/2, height, left_c1, left_c2, Y_AXIS);
   // setGradient(width/2, 0, width/2, height, right_c1, right_c2, X_AXIS);
   // image(c, 0, 0, width/2, height);
@@ -484,7 +588,7 @@ function printStory (node) {
   character = node.character;
   // **********************
   // location to print story
-  multiplexer += 0.05;
+  multiplexer += 0.025;
   if (node.flag == 't') {
     statChange(currentNode);
     if (node.npc === true) {
